b46ba859d5730a1c000a9266fcaaa20ca5b3e76c
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 17:26:36 -0400

    updated index.html

--- API/auth.php
+++ API/auth.php
@@ -6,7 +6,7 @@ if(isset($_GET['action']) && $_GET['action']=='signout' && isset($_SESSION['user
 	die(json_encode(['status'=>1,'message'=>'You have been signed out']));
 }
 if(isset($_SESSION['user/ID'])) {
-	die(json_encode(['status'=>-1,'message'=>'The user is already logged.','user_ID'=>$_SESSION['user/ID'],'is_admin'=>$_SESSION['user/is_admin']));
+	die(json_encode(['status'=>-1,'message'=>'The user is already logged.','user_ID'=>$_SESSION['user/ID'],'is_admin'=>$_SESSION['user/is_admin'],'firstname'=>$_SESSION['user/firstname'],'lastname'=>$_SESSION['user/lastname']));
 }
 
 if(count($_POST)>0){
@@ -49,7 +49,7 @@ die(json_encode(['status'=>-1,'message'=>'This route is invalid']));
 function signin($email,$password){
 	require(__DIR__.'/lib_db.php');
 	// Check if the user is in the database
-	$query=$pdo->prepare('SELECT ID,password,is_admin FROM users WHERE email=?');
+	$query=$pdo->prepare('SELECT ID,password,is_admin,firstname,lastname FROM users WHERE email=?');
 	$query->execute([$email]);
 	if($query->rowCount()==0){
 		die(json_encode(['status'=>-1,'message'=>'The user does not exist. Please, sign up.']));
@@ -59,6 +59,8 @@ function signin($email,$password){
 	if(password_verify($password,$user['password'])){
 		$_SESSION['user/ID']=$user['ID'];
 		$_SESSION['user/is_admin']=$user['is_admin'];
+		$_SESSION['user/firstname']=$user['firstname'];
+		$_SESSION['user/lastname']=$user['lastname'];
 		die(json_encode(['status'=>1,'message'=>'Welcome to our website']));
 	}else{
 		die(json_encode(['status'=>-1,'message'=>'The email or password are incorrect']));
