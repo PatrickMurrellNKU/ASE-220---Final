4331271d1a3dd9439e1e51e02fd0b8df2e7e1970
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 13:59:02 -0400

    updated auth.php with )

--- API/auth.php
+++ API/auth.php
@@ -18,20 +18,20 @@ if(count($_POST)>0){
 		
 	}
 }
-switch($_SERVER['REQUEST_METHOD']){
-	case 'GET':
-		if (isset($_SESSION['user/ID'])){
-			die(json_encode(['status'=>1,'message'=>'signed in']));
-		}else{
-			die(json_encode(['status'=>-1,'message'=>'signed out']));
-		}
-		break;
-	case 'POST': 
-		break;
-	case 'PUT':
-		break;
-	case 'DELETE':
-		break;
+// switch($_SERVER['REQUEST_METHOD']){
+// 	case 'GET':
+// 		if (isset($_SESSION['user/ID'])){
+// 			die(json_encode(['status'=>1,'message'=>'signed in']));
+// 		}else{
+// 			die(json_encode(['status'=>-1,'message'=>'signed out']));
+// 		}
+// 		break;
+// 	case 'POST': 
+// 		break;
+// 	case 'PUT':
+// 		break;
+// 	case 'DELETE':
+// 		break;
 }
 // if($_SERVER['REQUEST_METHOD'] == 'GET'){
 	// if (isset($_SESSION['user/ID']){
