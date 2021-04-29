961b656923d931b8e18709f8e824829c2c57444e
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 18:00:51 -0400

    updated index.html

--- API/auth.php
+++ API/auth.php
@@ -6,7 +6,6 @@ if(isset($_GET['action']) && $_GET['action']=='signout' && isset($_SESSION['user
 	die(json_encode(['status'=>1,'message'=>'You have been signed out']));
 }
 if(isset($_SESSION['user/ID'])) {
-	die(json_encode(['status'=>-1,'message'=>'The user is already logged.']));
 	die(json_encode(['status'=>-1,'message'=>'The user is already logged.','user_ID'=>$_SESSION['user/ID'],'is_admin'=>$_SESSION['user/is_admin']/*,'firstname'=>$_SESSION['user/firstname'],'lastname'=>$_SESSION['user/lastname']*/));
 }
 
@@ -46,39 +45,39 @@ die(json_encode(['status'=>-1,'message'=>'This route is invalid']));
 
 
 
-function signin($email,$password){
-	require(__DIR__.'/lib_db.php');
-	// Check if the user is in the database
-	$query=$pdo->prepare('SELECT ID,password,is_admin,firstname,lastname FROM users WHERE email=?');
-	$query->execute([$email]);
-	if($query->rowCount()==0){
-		die(json_encode(['status'=>-1,'message'=>'The user does not exist. Please, sign up.']));
-	}
-	//Check whether the password is correct
-	$user=$query->fetch();
-	if(password_verify($password,$user['password'])){
-		$_SESSION['user/ID']=$user['ID'];
-		$_SESSION['user/is_admin']=$user['is_admin'];
-		$_SESSION['user/firstname']=$user['firstname'];
-		$_SESSION['user/lastname']=$user['lastname'];
-		die(json_encode(['status'=>1,'message'=>'Welcome to our website']));
-	}else{
-		die(json_encode(['status'=>-1,'message'=>'The email or password are incorrect']));
-	}
-}
+// function signin($email,$password){
+// 	require(__DIR__.'/lib_db.php');
+// 	// Check if the user is in the database
+// 	$query=$pdo->prepare('SELECT ID,password,is_admin,firstname,lastname FROM users WHERE email=?');
+// 	$query->execute([$email]);
+// 	if($query->rowCount()==0){
+// 		die(json_encode(['status'=>-1,'message'=>'The user does not exist. Please, sign up.']));
+// 	}
+// 	//Check whether the password is correct
+// 	$user=$query->fetch();
+// 	if(password_verify($password,$user['password'])){
+// 		$_SESSION['user/ID']=$user['ID'];
+// 		$_SESSION['user/is_admin']=$user['is_admin'];
+// 		$_SESSION['user/firstname']=$user['firstname'];
+// 		$_SESSION['user/lastname']=$user['lastname'];
+// 		die(json_encode(['status'=>1,'message'=>'Welcome to our website']));
+// 	}else{
+// 		die(json_encode(['status'=>-1,'message'=>'The email or password are incorrect']));
+// 	}
+// }
 
-function signup($firstname,$lastname,$email,$password){
-	require(__DIR__.'/lib_db.php');
-	// Check if they already have an account
-	$query=$pdo->prepare('SELECT ID FROM users WHERE email=?');
-	$query->execute([$email]);
-	if($query->rowCount()>0){
-		echo 'The user already exists. Please, sign in.';
-		return;
-	}
-	//Add the user to the database
-	$query=$pdo->prepare('INSERT INTO users(email,password) VALUES(?,?)');
-	$query->execute([$firstname,$lastname,$email,password_hash($password, PASSWORD_DEFAULT)]);
-	echo 'Your account has been created. Please, sign in.';
-	//Show a message
-}
+// function signup($firstname,$lastname,$email,$password){
+// 	require(__DIR__.'/lib_db.php');
+// 	// Check if they already have an account
+// 	$query=$pdo->prepare('SELECT ID FROM users WHERE email=?');
+// 	$query->execute([$email]);
+// 	if($query->rowCount()>0){
+// 		echo 'The user already exists. Please, sign in.';
+// 		return;
+// 	}
+// 	//Add the user to the database
+// 	$query=$pdo->prepare('INSERT INTO users(email,password) VALUES(?,?)');
+// 	$query->execute([$firstname,$lastname,$email,password_hash($password, PASSWORD_DEFAULT)]);
+// 	echo 'Your account has been created. Please, sign in.';
+// 	//Show a message
+// }
