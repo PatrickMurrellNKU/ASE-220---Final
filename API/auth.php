20d63a4e1b9f43f23b6506518a97d55224684c70
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 18:33:01 -0400

    updated index.html

--- API/auth.php
+++ API/auth.php
@@ -11,7 +11,8 @@ if(isset($_SESSION['user/ID'])) {
 	'user_ID'=>$_SESSION['user/ID'],
 	'is_admin'=>$_SESSION['user/is_admin'],
 	'firstname'=>$_SESSION['user/firstname'],
-	'is_admin'=>$_SESSION['user/is_admin']
+	'is_admin'=>$_SESSION['user/is_admin'],
+	'lastname'=>$_SESSION['user/lastname'],
 	]));
 	//die(json_encode(['status'=>-1,'message'=>'The user is already logged.','user_ID'=>$_SESSION['user/ID'],'is_admin'=>$_SESSION['user/is_admin']));
 }
