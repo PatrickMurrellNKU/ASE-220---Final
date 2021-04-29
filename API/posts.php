1491972f50f631bbf0a6ee369560948c7af22ce4
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 12:42:05 -0400

    modified create posts api

--- API/posts.php
+++ API/posts.php
@@ -39,10 +39,10 @@ function detail($pdo){
 }
 
 function create($pdo){
-	if(/*!isset($_SESSION['user/ID'])*/false) die(json_encode(['status'=>-1,'message'=>'This page is for registered users only. Please <a href="auth.php">Sign in</a>.']));
+	if(!isset($_SESSION['user/ID'])) die(json_encode(['status'=>-1,'message'=>'This page is for registered users only. Please <a href="auth.php">Sign in</a>.']));
 	if(count($_POST)>0){
 		$stmt = $pdo->prepare('INSERT INTO post (title, description, link, user_ID) VALUES (?,?,?,?,?)');
-	$stmt->execute([$_POST['title'],$_POST['description'],$_POST['link'],/*$_SESSION['user/ID']]*/4);
+	$stmt->execute([$_POST['title'],$_POST['description'],$_POST['link'],$_SESSION['user/ID']]);
 		die(json_encode(['status'=>1,'message'=>'The message has been saved.']));
 	}
 }
