bb432a9493d58530093eb51f56b8fab37f1b599b
Patrick Murrell <patrick.murrell@educationatwork.org>
2021-04-29 19:53:55 -0400

    commit

--- API/auth.php
+++ API/auth.php
@@ -22,13 +22,14 @@ if(count($_POST)>0){
 			signin($_POST['email'],$_POST['password']);
 			break;
 		case 'signup':
-			die(json_encode(['firstname'=>$POST['firstname'],
-				'lastname'=>$POST['lastname'],
-				'post'=>$POST,
-				'email'=>$POST['email'],
-				'password'=>$POST['password']
-			]));
-			signup($POST['firstname'],$POST['lastname'],$_POST['email'],$_POST['password']);
+			// die(json_encode(['firstname'=>$POST['firstname'],
+			// 	'lastname'=>$POST['lastname'],
+			// 	'post'=>$POST,
+			// 	'email'=>$POST['email'],
+			// 	'password'=>$POST['password'],
+
+			// ]));
+			signup($_POST['firstname'],$_POST['lastname'],$_POST['email'],$_POST['password']);
 			break;
 	}
 }
